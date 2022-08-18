b1e948f13d601a90046775ea8825cc1963da34d5
choltz <choltz@cacbydesign.com>
2022-08-18 15:50:35 -0400

    8.18.315

--- Develop/script.js
+++ Develop/script.js
@@ -4,63 +4,63 @@ var myDay = [
         id: "0",
         hour: "09",
         time: "09",
-        meridiem: "am",
+        meridiem: " am",
         reminder: ""
     },
     {
         id: "1",
         hour: "10",
         time: "10",
-        meridiem: "am",
+        meridiem: " am",
         reminder: ""
     },
     {
         id: "2",
         hour: "11",
         time: "11",
-        meridiem: "am",
+        meridiem: " am",
         reminder: ""
     },
     {
         id: "3",
         hour: "12",
         time: "12",
-        meridiem: "pm",
+        meridiem: " pm",
         reminder: ""
     },
     {
         id: "4",
         hour: "01",
         time: "13",
-        meridiem: "pm",
+        meridiem: " pm",
         reminder: ""
     },
     {
         id: "5",
         hour: "02",
         time: "14",
-        meridiem: "pm",
+        meridiem: " pm",
         reminder: ""
     },
     {
         id: "6",
         hour: "03",
         time: "15",
-        meridiem: "pm",
+        meridiem: " pm",
         reminder: ""
     },
     {
         id: "7",
         hour: "04",
         time: "16",
-        meridiem: "pm",
+        meridiem: " pm",
         reminder: ""
     },
     {
         id: "8",
         hour: "05",
         time: "17",
-        meridiem: "pm",
+        meridiem: " pm",
         reminder: ""
     },
     
@@ -158,4 +158,5 @@ $(".saveBtn").on("click", function(event) {
     console.log(saveIndex);
     saveReminders();
     displayReminders();
+
 })
